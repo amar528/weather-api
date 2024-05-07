b52c9a2427f5640192561a3ab2f63d8f36e5fe99
amar528 <amar.om@icloud.com>
2024-05-07 17:56:46 +1000

    Open Weather json / SerDes

--- src/main/java/au/com/vanguard/demo/weatherapi/client/OpenWeatherResponse.java
+++ src/main/java/au/com/vanguard/demo/weatherapi/client/OpenWeatherResponse.java
@@ -1,11 +1,13 @@
 package au.com.vanguard.demo.weatherapi.client;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import lombok.Data;
 
 import java.util.List;
 import java.util.ArrayList;
 
 @Data
+@JsonIgnoreProperties(ignoreUnknown = true)
 public class OpenWeatherResponse {
 
     private List<Weather> weather = new ArrayList<>();
--- src/test/java/au/com/vanguard/demo/weatherapi/client/OpenWeatherResponseSerDesTest.java
+++ src/test/java/au/com/vanguard/demo/weatherapi/client/OpenWeatherResponseSerDesTest.java
@@ -3,7 +3,13 @@ package au.com.vanguard.demo.weatherapi.client;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.ObjectWriter;
+import org.json.JSONException;
 import org.junit.jupiter.api.Test;
+import org.skyscreamer.jsonassert.JSONAssert;
+import org.skyscreamer.jsonassert.JSONCompareMode;
+
+import java.io.InputStream;
+import java.nio.file.Paths;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -11,12 +17,14 @@ class OpenWeatherResponseSerDesTest {
 
     private final ObjectMapper mapper = new ObjectMapper();
 
+
     @Test
-    void shouldSerialize() throws JsonProcessingException {
+    void shouldSerialize() throws JsonProcessingException, JSONException {
         var underTest = new OpenWeatherResponse();
 
 
         var weather = new Weather();
+        underTest.getWeather().add(weather);
         weather.setId("1");
         weather.setIcon("09D");
         weather.setMain("Drizzle");
@@ -24,6 +32,13 @@ class OpenWeatherResponseSerDesTest {
 
         var json = mapper.writeValueAsString(underTest);
         assertNotNull(json);
+    }
+
+    @Test
+    void shouldDeserialize() throws Exception {
+
+        var result = mapper.readValue(Paths.get("target/test-classes/open-weather-response-1.json").toFile(), OpenWeatherResponse.class);
+        assertNotNull(result);
     }
 
 }
