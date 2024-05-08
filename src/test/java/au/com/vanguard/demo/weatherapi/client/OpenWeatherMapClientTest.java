dd3543cabbdbe9c5e0789455563146997e716366
amar528 <amar.om@icloud.com>
2024-05-08 10:09:57 +1000

    Wiremock testing

--- src/test/java/au/com/vanguard/demo/weatherapi/client/OpenWeatherMapClientTest.java
+++ src/test/java/au/com/vanguard/demo/weatherapi/client/OpenWeatherMapClientTest.java
@@ -2,7 +2,6 @@ package au.com.vanguard.demo.weatherapi.client;
 
 import com.github.tomakehurst.wiremock.WireMockServer;
 import com.github.tomakehurst.wiremock.client.WireMock;
-import com.github.tomakehurst.wiremock.matching.StringValuePattern;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -15,11 +14,11 @@ import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 
 import java.io.IOException;
-import java.util.HashMap;
-import java.util.Map;
 
 import static com.github.tomakehurst.wiremock.client.WireMock.*;
 import static java.nio.charset.Charset.defaultCharset;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.springframework.util.StreamUtils.copyToString;
 
 @SpringBootTest
@@ -36,20 +35,11 @@ class OpenWeatherMapClientTest {
     private OpenWeatherMapClient underTest;
 
     static void setupMockOpenWeatherResponse(WireMockServer mockService, HttpStatus status, String filePath, String appId, String queryParams) throws IOException {
-        mockService.stubFor(
-                WireMock.get(
-                                WireMock.urlPathTemplate("/data/2.5/weather"))
-                        .withQueryParam("appid", havingExactly(equalTo(appId)))
-                        .withQueryParam("q", havingExactly(containing(queryParams)))
-                        .willReturn(WireMock.aResponse()
-                                .withStatus(status.value())
-                                .withHeader("Content-Type", MediaType.APPLICATION_JSON_VALUE)
-                                .withBody(copyToString(OpenWeatherMapClientTest.class.getClassLoader()
-                                        .getResourceAsStream(filePath), defaultCharset()))));
+        mockService.stubFor(WireMock.get(WireMock.urlPathTemplate("/data/2.5/weather")).withQueryParam("appid", havingExactly(equalTo(appId))).withQueryParam("q", havingExactly(containing(queryParams))).willReturn(WireMock.aResponse().withStatus(status.value()).withHeader("Content-Type", MediaType.APPLICATION_JSON_VALUE).withBody(copyToString(OpenWeatherMapClientTest.class.getClassLoader().getResourceAsStream(filePath), defaultCharset()))));
     }
 
     @Test
-    void shouldReturnValidWeatherData_givenValidOpenWeatherResponse() throws Exception {
+    void givenValidOpenWeatherResponse_shouldReturnValidWeatherData() throws Exception {
 
         // given valid request and valid response and body (200 OK with valid json body)
         var appId = "abcde01234";
@@ -59,6 +49,21 @@ class OpenWeatherMapClientTest {
 
         // when
         OpenWeatherResponse openWeatherResponse = underTest.findWeatherData(appId, queryParams);
+
+        // then
+        assertNotNull(openWeatherResponse);
+        assertNotNull(openWeatherResponse.getWeather());
+        assertEquals(1, openWeatherResponse.getWeather().size());
+        var weatherResponse = openWeatherResponse.getWeather().get(0);
+        assertEquals("300", weatherResponse.getId());
+        assertEquals("Drizzle", weatherResponse.getMain());
+        assertEquals("light intensity drizzle", weatherResponse.getDescription());
+
+        mockOpenWeatherService.verify(exactly(1),
+                getRequestedFor(urlPathMatching("/data/2.5/weather/*"))
+                        .withQueryParam("appid", equalTo(appId))
+                        .withQueryParam("q", havingExactly("Melbourne, AUS")));
+
     }
 
 }
