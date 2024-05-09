dc02be298ea91b31a5745c1f08d2cd2abcedb07f
amar528 <amar.om@icloud.com>
2024-05-09 11:39:24 +1000

    Additional unit testing

--- src/test/java/au/com/vanguard/demo/weatherapi/model/WeatherDataTest.java
+++ src/test/java/au/com/vanguard/demo/weatherapi/model/WeatherDataTest.java
@@ -3,6 +3,7 @@ package au.com.vanguard.demo.weatherapi.model;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotEquals;
 
 class WeatherDataTest {
 
@@ -15,11 +16,27 @@ class WeatherDataTest {
     }
 
     @Test
-    void shouldHash() {
+    void shouldNotEquals() {
+        var weatherData = new WeatherDataBuilder().city("city").country("country").description("Hazy sunshine").build();
+        var weatherData2 = new WeatherDataBuilder().city("city2").country("country2").description("Rain").build();
+
+        assertNotEquals(weatherData, weatherData2);
+    }
+
+    @Test
+    void shouldHashEqual() {
         var weatherData = new WeatherDataBuilder().city("city").country("country").description("Hazy sunshine").build();
         var weatherData2 = new WeatherDataBuilder().city("city").country("country").description("Hazy sunshine").build();
 
         assertEquals(weatherData.hashCode(), weatherData2.hashCode());
+    }
+
+    @Test
+    void shouldNotHashEqual() {
+        var weatherData = new WeatherDataBuilder().city("city").country("country").description("Hazy sunshine").build();
+        var weatherData2 = new WeatherDataBuilder().city("city2").country("country2").description("Rain").build();
+
+        assertNotEquals(weatherData.hashCode(), weatherData2.hashCode());
     }
 
 }
